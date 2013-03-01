209ba1b46a8898c17fdb2b7a016d8ba152c84adc
Sara Van Buskirk <svanbusk@unca.edu>
2013-02-28 21:58:53 -0500

    Get tag search operational.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,16 +1,25 @@
 var main = function () {
   "use strict";
   var twitter = new ctwitter.CTwitter();
-  twitter.stream("statuses/filter", { lang: "en", track: ["javascript", "jslint", "jquery"] }, function (stream) {
-    stream.on("data", function (tweet) {
-      $(".jcarousel").jcarousel({
-        "vertical": null,
-      })
-      .jcarouselAutoscroll({
-        "interval": 900
+
+  $("#tag").val("Enter a tag here!");
+
+  $("#submit").click(function () {
+    $("#after-submit .tracking h3").append( $("#tag").val() );
+
+    $("#before-submit").fadeOut(500, function () {
+      $("#after-submit").fadeIn(500);
+    });
+    twitter.stream("statuses/filter", { lang: "en", track: $("#tag").val() }, function (stream) {
+      stream.on("data", function (tweet) {
+        $(".jcarousel").jcarousel({
+          "vertical": true
+        }).jcarouselAutoscroll({
+          "interval": 900
+        });
+        $(".jcarousel ul").append("<li> <h4>" + tweet.from_user + "</h4>" + "<p>" + tweet.text + "</p> </li>");
+        $(".jcarousel").jcarousel('reload');
       });
-      $(".jcarousel ul").append("<li> <h4>" + tweet.from_user + "</h4>" + "<p>" + tweet.text + "</p> </li>");
-      $(".jcarousel").jcarousel('reload');
     });
   });
 };
