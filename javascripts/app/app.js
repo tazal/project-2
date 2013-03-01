ad2768f077a56dc685c7a1edd59fb5595dcd0908
Sara Van Buskirk <svanbusk@unca.edu>
2013-02-28 22:47:39 -0500

    Update code to be in line with JSLint expectations.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,27 +1,34 @@
-var main = function () {
+/*global window*/
+(function () {
   "use strict";
-  var twitter = new ctwitter.CTwitter();
+  var $ = window.$,
+    document = window.document,
+    ctwitter = window.ctwitter,
+    main = function () {
+      var twitter = new ctwitter.CTwitter(),
+        userInput;
 
-  $("#tag").val("Enter a tag here!");
+      $("#submit").click(function () {
+        userInput = $("#tag").val();
+        $("#after-submit .tracking h3").append(userInput);
 
-  $("#submit").click(function () {
-    $("#after-submit .tracking h3").append( $("#tag").val() );
-
-    $("#before-submit").fadeOut(500, function () {
-      $("#after-submit").fadeIn(500);
-    });
-    twitter.stream("statuses/filter", { lang: "en", track: $("#tag").val() }, function (stream) {
-      stream.on("data", function (tweet) {
-        $(".jcarousel").jcarousel({
-          "vertical": true
-        }).jcarouselAutoscroll({
-          "interval": 900
+        $("#before-submit").fadeOut(500, function () {
+          $("#after-submit").fadeIn(500);
         });
-        $(".jcarousel ul").append("<li> <h4>" + tweet.from_user + "</h4>" + "<p>" + tweet.text + "</p> </li>");
-        $(".jcarousel").jcarousel('reload');
+        twitter.stream("statuses/filter", { lang: "en", track: userInput}, function (stream) {
+          stream.on("data", function (tweet) {
+            $(".jcarousel").jcarousel({
+              "vertical": true
+            }).jcarouselAutoscroll({
+              "interval": 900
+            });
+            $(".jcarousel ul").append("<li> <h4>" + tweet.from_user + "</h4>" + "<p>" + tweet.text + "</p> </li>");
+            $(".jcarousel").jcarousel('reload');
+          });
+        });
       });
-    });
-  });
-};
+    };
 
-$(document).ready(main);
+  $(document).ready(main);
+
+}());
